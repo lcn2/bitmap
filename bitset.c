8f986faadd5902f4eb93f1d4ab5b11b16a024d47
Landon Curt Noll <github-mail@asthe.com>
2001-12-27 09:47:38 +0000

    chkpt - pre-compile --chongo

--- bitset.c
+++ bitset.c
@@ -39,7 +39,7 @@
  *		start + step * (x*8 + y)
  *
  *	The value 'v', is represented by octet 'x' bit 'y':
- *	provided that (v - start) == 0 mod step.  (i.e., the value
+ *
  *		t = (v - start) / step
  *		x = t / 8
  *		y = t % 8
@@ -69,6 +69,13 @@
  * non-ignored input value, relative to the start value.  The bitmap
  * is always written in whole octets, with the extra padding bits being
  * filled with zeros.  So it is possible that up to 7 extra 0 bits may be
+ * written to stdout.
+ *
+ * The program operates on one bitmap buffer at a time.  The buffer
+ * contains BUFSIZ octets where BUFSIZ is defined by <stdio.h>.
+ * Frequently BUFSIZ is 8k octets.
+ *
+ * If the input is malformed (cannot be converted into a signed long long)
  * then an warning message will be sent to stderr.  If the input value is <
  * the previous non-ignored input value (unsorted), an warning message will
  * be sent to stderr.  However duplicate values, values < start and
@@ -80,8 +87,16 @@
 
 /*
  * maximum useful input line
+ */
+#define MAXLINE (19+1)	/* 2^63-1 is 19 digits long + newline */
+
+u_int8_t buffer[BUFSIZ+1];
+ * a BUFSIZ chunk of the output bitmap, with an extra guard octet for safety
+ *
+ * This is the current bitmap buffer with a first bit value of 'bottom'.
+ */
 static u_int8_t buffer[BUFSIZ+1];
-u_int8_t bitmap[BUFSIZ+1];
+u_int8_t zero[BUFSIZ+1];
 /*
 
 /*
@@ -91,45 +106,65 @@ char *program;
 
 int
 main(int argc, char *argv[])
-    int64_t bottom;		/* low bit value of buffer */
-    int64_t span;		/* range of values spanned by a buffer */
-    int64_t beyond;		/* value of bit just beyond end of buffer */
+{
+    int64_t start;		/* starting bitmap value */
+    int64_t step;		/* bitmap increment value */
+    int64_t value;		/* input value from stdin */
+    int64_t line;		/* input line number */
+    int64_t bottom;		/* low bit value of bitmap */
+    int64_t span;		/* range of values spanned by a bitmap */
+    int64_t beyond;		/* value of bit just beyond end of bitmap */
     int had_prev;		/* 1 ==> seen a previous non-ignored value */
     int64_t prev;		/* previous non-ignored value */
     int64_t boffset;		/* total bit offset in buffer for value */
     int octet;			/* octet offset in buffer for value */
     int bit;			/* bit offset in byte for value */
 
-	fprintf(stderr, "usage: %s start step\n", argv[0]);
-	exit(0);
+	fprintf(stderr, "usage: %s start step\n", __program);
+     * parse args
      */
+    program = argv[0];
     if (argc != 3) {
+	fprintf(stderr, "usage: %s start step\n", program);
+	exit(1);
+		__program);
+    errno = 0;
+    start = strtoll(argv[1], NULL, 0);
+    if (errno == ERANGE) {
 	fprintf(stderr, "%s: start value must be [-(2^63),(2^63-1)]\n",
+		program);
+	fprintf(stderr, "%s: start value must be [1,(2^63-1)]\n", __program);
+    }
+    errno = 0;
     step = strtoll(argv[2], NULL, 0);
-	fprintf(stderr, "%s: step: %lld must be > 0\n", argv[0], step);
-	exit(2);
+	fprintf(stderr, "%s: step: %lld must be > 0\n", __program, step);
+	fprintf(stderr, "%s: start value must be [1,(2^63-1)]\n", program);
         exit(3);
     }
     if (step <= 0) {
 	fprintf(stderr, "%s: step: %lld must be > 0\n", program, step);
 	exit(4);
     }
-     * for the buffer to extend beyond the max (2^63-1) value.  If this
+
      * ever happens, then bottom > beyond.  This isn't a big problem becase
      * setup and initialize
      *
      * There is some deep magic in the beyond calculation.  It is possible
      * for the bitmap buffer to extend beyond the max (2^63-1) value.  If this
      * ever happens, then bottom > beyond.  This isn't a big problem because
-    memset(inbuf, '\0', BUFSIZ+1);
+     * at some point the input values will either overflow the range
+     * or become unsorted.  Either was the non-ignored input will stop at
+     * or before the highest possible bit value.
      */
     line = 0;	/* 1st line will be 1 */
     memset(inbuf, '\0', MAXLINE+1);
+    had_prev = 0;	/* no previos non-ignored value */
     bottom = start;
     span = 8*BUFSIZ*step;
     beyond = start + span;
     had_prev = 0;	/* no previous non-ignored value */
-    while (fgets(inbuf, MAXLINE+1, stdin) == NULL) {
+    prev = 0;
+
     /*
      * output sieve buffers until EOF
      */
@@ -156,10 +191,10 @@ main(int argc, char *argv[])
 	while (*p != '\0' && isdigit(*p) && p < inbuf+MAXLINE) {
 	    ++p;
 	}
-			argv[0], line);
+			__program, line);
 	if (*p != '\n' || *(p+1) != '\0') {
 	    /* improper line, ignore it */
-			argv[0], line);
+			__program, line);
 		fprintf(stderr, "%s: line %lld: ignoring, line too long\n",
 			program, line);
 	    } else {
@@ -171,10 +206,170 @@ main(int argc, char *argv[])
 
     	/*
 	 * convert input into an input value
-		    argv[0], line);
+		    __program, line);
 	errno = 0;
 	value = strtoll(inbuf, NULL, 0);
+	if (errno == ERANGE) {
+	    fprintf(stderr, "%s: line %lld: ignoring, value out of range\n",
+		    program, line);
+	    continue;
+	}
+
+	/*
+			__program, line);
+	 */
+	if (had_prev && value <= prev) {
+	    if (value < prev) {
+		fprintf(stderr, "%s: line %lld: ignoring, value not sorted\n",
+			program, line);
+	    }
+	    continue;
+	}
+
+	/*
+	 * silently ignore if below start
+	 */
+	 * silently ignore if not a bitmap representable value
+	    continue;
+	}
+
+	/*
+	 * silently ignore if not a bitmap potential value
+	 */
+	if (((value - start) % step) != 0) {
+	    continue;
+	}
+
+	/*
+	 * At this point we know that the value will cause us to set a
+	 * bit somewhere in the bitmap, the question is where.  It could
+	 * be on the current bitmap.  It could be in some future bit map
+	 * causing is to have to write this bitmap, followed by 0 or more
+	 * 0-filled bitmaps before being able to set the bit in the new
+	 * bitmap.
+	 */
+
+    	/*
+	 * case: value is beyond current bitmap
+	 *
+	 * NOTE: We must check for beyond > bottom because the current
+	 *	 bitmap buffer could go beyond 2^63-1.
+	 */
+    	if (beyond > bottom && value >= beyond) {
+
+	    /*
+			__program, strerror(errno));
+	     */
+	    errno = 0;
+	    if (fwrite(buffer, 1, BUFSIZ, stdout) != BUFSIZ) {
+		fprintf(stderr, "%s: buffer write error: %s\n",
+			program, strerror(errno));
+		exit(5);
+	    }
+
+	    /*
+	     * If there is a large gap, then we may need to write out
+	     * 1 or more zero filled buffers.  In any event we must
+	     * at least update the bitmap buffer 'bottom' and 'beyond' values.
+	     */
+		/* 
+		 * determine if 0-filled bitmap buffer needs to be written 
+		bottom = beyond;
+		beyond += span;
+
+		/*
+		 * determine if 0-filled bitmap buffer needs to be written
+		 *
+		 * NOTE: We must check for beyond > bottom because the current
+		 *	 bitmap buffer could go beyond 2^63-1.
+		 */
+		if (beyond > bottom && value >= beyond) {
+
+		    /*
+				__program, strerror(errno));
+		     */
+		    errno = 0;
+		    if (fwrite(zero, 1, BUFSIZ, stdout) != BUFSIZ) {
+			fprintf(stderr, "%s: 0-buffer write error: %s\n",
+				program, strerror(errno));
+			exit(6);
+		    }
+		}
+	    /* NOTE: beyond > bottom magic again */
+	    } while(beyond > bottom && value >= beyond);
+
+	    /*
+	     * We have just written our older bitmap buffer, so we must zero it
+	     * out for the new range to use.
+	     */
+	    memset(buffer, '\0', BUFSIZ+1);
+	}
+
+	/*
+	 * At this point we know that we need to set a bit in the current
+	 * bitmap buffer.  We will now determine where the bit to be set
+	 * resides.
+		    __program, boffset, BUFSIZ*8);
+	boffset = (value - start) / step;
+	/* firewall */
+	if (boffset > BUFSIZ) {
+	    fprintf(stderr, "%s: FATAL: unexpected bit offset: %lld > %d\n",
+		    program, boffset, BUFSIZ*8);
+	    exit(7);
+	}
+	octet = (int)(boffset / 8);
+	bit = (int)(boffset % 8);
+
+	/*
+	 * Set the bit ... this is where the useful work is done!  :-)
+	 */
+	buffer[octet] |= (1<<bit);
+
+	/*
+	 * note that we have a (perhaps new) non-ignored previous value
+	 */
+    	had_prev = 1;
+	prev = value;
+	/* clear any error flags */
+	errno = 0;
+    }
+	fprintf(stderr, "%s: read error: %s\n", __program, strerror(errno));
+    /*
      * firewall - catch the case of stdin error
+     */
+    if (ferror(stdin)) {
+	fprintf(stderr, "%s: read error: %s\n", program, strerror(errno));
+	exit(8);
+    }
+
+    /*
+     * We have reached the end of input, so it is time to output the
+     * current and partial bitmap buffer.  It is possible that we did not find
+     * any values, and thus the buffer will be empty.  It is also possible
+     * that we will write only a few of the bitmap buffer octets as we
+     * will stop writing at the last octet for which there is a 1 bit.
+    for (octet = BUFSIZ-1; octet >= 0 && bitmap[octet] == 0; --octet) {
+
+    /*
+     * determine the highest octet for which there is a 1 bit, it any
+     */
+    for (octet = BUFSIZ-1; octet >= 0 && buffer[octet] == 0; --octet) {
+    }
+
+    /*
+     * write out only the bitmap octets that are needed, if any
+     */
+		    __program, strerror(errno));
+	errno = 0;
+	if (fwrite(buffer, 1, octet+1, stdout) != octet+1) {
+	    fprintf(stderr, "%s: final buffer write error: %s\n",
+		    program, strerror(errno));
+	    exit(9);
+	}
+    }
+
+    /*
+     * All done!
      *
      * 	-- Jessica Noll, 1985
      */
