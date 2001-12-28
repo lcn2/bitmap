2573c51e188507b619daeaa58be14f3b7547641c
Landon Curt Noll <github-mail@asthe.com>
2001-12-28 06:44:21 +0000

    updated header --chongo

--- bitset.c
+++ bitset.c
@@ -85,6 +85,36 @@
  * be silently ignored.
  */
 
+/*
+ * @(#) $Revision$
+ * @(#) $Id$
+ * @(#) $Source$
+ *
+ * Copyright (c) 2001 by Landon Curt Noll.  All Rights Reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software and
+ * its documentation for any purpose and without fee is hereby granted,
+ * provided that the above copyright, this permission notice and text
+ * this comment, and the disclaimer below appear in all of the following:
+ *
+ *       supporting documentation
+ *       source copies
+ *       source works derived from this source
+ *       binaries derived from this source or from derived source
+ *
+ * LANDON CURT NOLL DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO
+ * EVENT SHALL LANDON CURT NOLL BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF
+ * USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ *
+ * chongo <was here> /\oo/\
+ *
+ * Share and enjoy!
+ */
+
 #include <stdio.h>
 #include <ctype.h>
 #include <errno.h>
